4f95f0b41d32070caf27f1aad68ac973ac5baa3f
ide-la-r <ism17v2@gmail.com>
2025-04-09 20:43:14 +0200

    update of products with petitions and body request

--- 06-ExpressJS/arquitectura/PrimeraApp/routes/products.js
+++ 06-ExpressJS/arquitectura/PrimeraApp/routes/products.js
@@ -4,11 +4,18 @@ router.get('/', (req, res) =>{
     res.send('Recuperamos todos los productos');
 });
 
+router.get('/:productId', (req, res) =>{
+    const { productId } = req.params;//aqui estaran todos los valores que estaran en la URL en forma de clave/valor
+    res.send(`Recupero el producto con ID: ${productId}`);
+});
+
 router.get('/new', (req, res) =>{
     res.send('Formulario para crear productos');
 });
 
 router.post('/create', (req, res) =>{
+    const { name, price } = req.body;//aqui tenemos los datos de el json de la peticion
+    console.log(name, price);
     res.send('Gestion de los datos del formulario');
 });
 
